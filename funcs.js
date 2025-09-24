80076bc323a568e02be74f23abc5adc633db3613
Kiyan Alav <kiyanalavi1379@gmail.com>
2025-09-24 18:41:15 +0330

    course crud

--- funcs.js
+++ funcs.js
@@ -6,3 +6,8 @@ console.log("Kiyan2");
 const login = () => true;
 const register = () => false;
 const getMe = () => true;
+
+const removeCourse = (id) => id;
+const addCourse = () => true;
+const editCourse = () => true;
+const getCourse = () => true;
